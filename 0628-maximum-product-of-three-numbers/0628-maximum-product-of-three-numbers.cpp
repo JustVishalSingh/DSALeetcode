e7c6c4ac43b065738b679a74241961e2b67eb628
JustVishalSingh <justvishalkumarsingh1275@gmail.com>
2026-07-31 21:20:30 +0530

    Time: 0 ms (100%), Space: 31.6 MB (12.8%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -4,7 +4,34 @@ public:
         int num=arr.size();
         int n=num-1;
         int maxp=INT_MIN;
-        sort(arr.begin(), arr.end());
-        return max(arr[n]* arr[n-1]*arr[n-2], arr[0]*arr[1]*arr[n]);
+        int max1=INT_MIN;
+        int max2=INT_MIN;
+        int max3=INT_MIN;
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i]>max1){
+                max3=max2;
+                max2=max1;
+                max1=arr[i];
+            }
+            else if(arr[i]>max2){
+                max3=max2;
+                max2=arr[i];
+            }
+            else if(arr[i]>max3){
+                max3=arr[i];
+            }
+        }
+        int min1=INT_MAX;
+        int min2=INT_MAX;
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i]<min1){
+                min2=min1;
+                min1=arr[i];
+            }
+            else if(arr[i]<min2){
+                min2=arr[i];
+            }
+        }
+        return max(max1*max2*max3, min1*min2*max1);
     }
 };

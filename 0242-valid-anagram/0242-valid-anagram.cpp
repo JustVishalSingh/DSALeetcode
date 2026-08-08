6cab5ff0a0b7cc9b544bc9cb8fdd26dea2ec691f
JustVishalSingh <justvishalkumarsingh1275@gmail.com>
2026-08-08 20:56:32 +0530

    Time: 8 ms (6.93%), Space: 9.6 MB (76.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        for (char &c : s){
-        c = tolower(c);
-    }
-        sort(s.begin(), s.end());
+        // for (char &c : s){
+        // c = tolower(c);
+    //}
+        sort(s.begin(),s.end());
 
-        for (char &c : t){
-        c = tolower(c);
-    }
+        // for (char &c : t){
+        // c = tolower(c);
+    //}
         sort(t.begin(), t.end());
         if(t==s){
             return true;

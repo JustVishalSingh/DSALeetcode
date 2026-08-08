0a9a83bdf2a0a57fd2f7719bc1bd868a6146531c
JustVishalSingh <justvishalkumarsingh1275@gmail.com>
2026-08-08 20:57:49 +0530

    Time: 7 ms (18.36%), Space: 9.7 MB (49.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // for (char &c : s){
-        // c = tolower(c);
-    //}
         sort(s.begin(),s.end());
-
-        // for (char &c : t){
-        // c = tolower(c);
-    //}
         sort(t.begin(), t.end());
-        if(t==s){
-            return true;
-        }
-        return false;
+        return s==t;
     }
 };

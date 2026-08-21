bfea38c063c63a87d6ddce41793caf4d4253fed9
JustVishalSingh <justvishalkumarsingh1275@gmail.com>
2026-08-21 23:37:11 +0530

    Time: 0 ms (100%), Space: 15.1 MB (69.05%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
-    int pivote(vector<int>&arr){
-        int s=0;
-        int e=arr.size()-1;
-        while(s<e){
-            int mid= s+(e-s)/2;
-            if(arr[mid]>=arr[0]){
-                s=mid+1;
-            }
-            else{
-                e=mid;
-            }
-        }
-        return s;
-    }
-    int binarySerch(vector<int>& arr, int target, int s, int e){
+    int search(vector<int>& nums, int target) {
+        int s=0, e=nums.size()-1;
         while(s<=e){
-            int mid= s+(e-s)/2;
-            if(arr[mid]==target){
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target){
                 return mid;
             }
-            else if(arr[mid]<target){
-                s=mid+1;
+            else if(nums[s]<=nums[mid]){
+                if(nums[s]<=target && target<nums[mid]){
+                    e=mid-1;
+                }
+                else{
+                    s=mid+1;
+                }
             }
             else{
-                e=mid-1;
+                if(nums[mid]<nums[e]){
+                    if(nums[mid]<target&&target<=nums[e]){
+                        s=mid+1;
+                    }
+                    else{
+                        e=mid-1;
+                    }
+                }
             }
         }
         return -1;
-    }
-    int search(vector<int>& nums, int target) {
-       int  pv=pivote(nums);
-        int e=nums.size()-1;
-        int left=binarySerch(nums, target,0, pv-1);
-        if(left!=-1){
-            return left;
-        }
-        return binarySerch(nums, target,pv, e);
     }
 };

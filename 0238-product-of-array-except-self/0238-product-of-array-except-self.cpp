e44f85d6d4053628b78b06d9e9086b8dfd4f92f5
JustVishalSingh <justvishalkumarsingh1275@gmail.com>
2026-08-26 00:21:53 +0530

    Time: 3 ms (37.29%), Space: 40.2 MB (81.23%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -7,14 +7,10 @@ public:
         for(int i=1; i<n; i++){
             ans[i]=ans[i-1]*nums[i-1];
         }
-        vector<int>suff(n);
-        int idx=n-1;
-        suff[idx]=1;
-        for(int i=n-2; i>=0;i--){
-            suff[i]=suff[i+1]*nums[i+1];
-        }
-        for(int i=0; i<n;i++){
-            ans[i]=ans[i]*suff[i];
+        int suffix=1;
+        for(int i=n-1; i>=0; i--){
+            ans[i]*=suffix;
+            suffix*=nums[i];
         }
         return ans;
     }

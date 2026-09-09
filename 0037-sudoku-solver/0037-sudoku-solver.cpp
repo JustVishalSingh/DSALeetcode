e8007121d29817ac8af6c5a1b4a50941b193f533
JustVishalSingh <justvishalkumarsingh1275@gmail.com>
2026-09-10 02:03:30 +0530

    Time: 42 ms (97.94%), Space: 9 MB (24.37%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,59 +1,63 @@
 class Solution {
 public:
-    bool isSafe(vector<vector<char>>& board, int row,int col, char dig){
-        for(int i=0; i<9; i++){
-            if(board[row][i]==dig){
-                return false;
-            }
-        }
-        for(int i=0; i<9; i++){
-            if(board[i][col]==dig){
-                return false;
-            }
-        }
-        int str=(row/3)*3;
-        int stc= (col/3)*3;
+    int rowMask[9] = {};
+    int colMask[9] = {};
+    int boxMask[9] = {};
 
-        for(int i=str; i<=str+2;i++){
-            for(int j=stc; j<=stc+2; j++){
-                if(board[i][j]==dig){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-
-    bool helper(vector<vector<char>>& board, int row, int col){
-        if(row==9){
+    bool helper(vector<vector<char>>& board, int idx) {
+        if (idx == 81)
             return true;
+
+        int row = idx / 9;
+        int col = idx % 9;
+
+        if (board[row][col] != '.')
+            return helper(board, idx + 1);
+
+        int box = (row / 3) * 3 + (col / 3);
+
+        for (int d = 0; d < 9; d++) {
+            int bit = 1 << d;
+
+            if ((rowMask[row] & bit) ||
+                (colMask[col] & bit) ||
+                (boxMask[box] & bit))
+                continue;
+
+            board[row][col] = '1' + d;
+
+            rowMask[row] |= bit;
+            colMask[col] |= bit;
+            boxMask[box] |= bit;
+
+            if (helper(board, idx + 1))
+                return true;
+
+            rowMask[row] ^= bit;
+            colMask[col] ^= bit;
+            boxMask[box] ^= bit;
+
+            board[row][col] = '.';
         }
 
-        int nextRow=row, nextCol=col+1;
-        if(nextCol==9){
-            nextRow=row+1;
-            nextCol=0;
-        }
-
-        if(board[row][col]!='.'){
-            return helper(board, nextRow, nextCol);
-        }
-
-        for(char dig='1'; dig<='9'; dig++){
-            if(isSafe(board, row , col, dig)){
-                board[row][col]=dig;
-                if(helper(board, nextRow, nextCol)){
-                    return true;
-                }
-                board[row][col]='.';
-            }
-        }
         return false;
     }
 
-
     void solveSudoku(vector<vector<char>>& board) {
-        helper(board, 0, 0);
+        // Initialize masks
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int d = board[i][j] - '1';
+                    int bit = 1 << d;
+
+                    rowMask[i] |= bit;
+                    colMask[j] |= bit;
+                    boxMask[(i / 3) * 3 + (j / 3)] |= bit;
+                }
+            }
+        }
+
+        helper(board, 0);
     }
 };
